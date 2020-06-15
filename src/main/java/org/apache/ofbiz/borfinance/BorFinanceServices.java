b2f2401f8c5f8173f55e51cfe6ae1978a902746d
bordyale <alessio.bordignon@gmail.com>
2020-06-15 14:18:47 +0200

    populate dividend table

---
+++ src/main/java/org/apache/ofbiz/borfinance/BorFinanceServices.java
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *******************************************************************************/
+
+package org.apache.ofbiz.borfinance;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.UtilDateTime;
+import org.apache.ofbiz.base.util.UtilMisc;
+import org.apache.ofbiz.base.util.UtilNumber;
+import org.apache.ofbiz.base.util.UtilProperties;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.service.GenericServiceException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityExpr;
+import org.apache.ofbiz.entity.condition.EntityOperator;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.LocalDispatcher;
+import org.apache.ofbiz.service.ModelService;
+import org.apache.ofbiz.service.ServiceUtil;
+import org.json.JSONObject;
+
+/**
+ * Services for Agreement (Accounting)
+ */
+
+public class BorFinanceServices {
+
+	public static final String module = BorFinanceServices.class.getName();
+
+	public static final String resource = "BorFinanceUiLabels";
+	private static final String USER_AGENT = "Mozilla/5.0";
+
+	private static final String[] apikey = { "IJMOD1FFWEBG5VWY", "IJMOD1FFWEBG5VWY", "IJMOD1FFWEBG5VWY", "IJMOD1FFWEBG5VWY", "IJMOD1FFWEBG5VWY", "TCWC4KTESJIY8UL5", "TCWC4KTESJIY8UL5",
+			"TCWC4KTESJIY8UL5", "TCWC4KTESJIY8UL5", "TCWC4KTESJIY8UL5", "WRFKAPP9TCNWQUKC", "WRFKAPP9TCNWQUKC", "WRFKAPP9TCNWQUKC", "WRFKAPP9TCNWQUKC", "WRFKAPP9TCNWQUKC", "NRXCKC71QSMJQZYA",
+			"NRXCKC71QSMJQZYA", "NRXCKC71QSMJQZYA", "NRXCKC71QSMJQZYA", "NRXCKC71QSMJQZYA" };
+
+	public static Map<String, Object> populateDividendTable(DispatchContext ctx, Map<String, Object> context) {
+		Delegator delegator = ctx.getDelegator();
+		LocalDispatcher dispatcher = ctx.getDispatcher();
+		Locale locale = (Locale) context.get("locale");
+		String errMsg = null;
+		Map<String, Object> result = ServiceUtil.returnSuccess();
+
+		GenericValue userLogin = (GenericValue) context.get("userLogin");
+
+		try {
+			List<EntityExpr> exprs = UtilMisc.toList(EntityCondition.makeCondition("productType", EntityOperator.EQUALS, "STOCK"));
+			EntityCondition cond = EntityCondition.makeCondition(exprs, EntityOperator.AND);
+			List<GenericValue> conditions = EntityQuery.use(delegator).from("BfinProduct").where(cond).queryList();
+
+			int i = 0;
+			int apiindex = 0;
+			boolean reset = false;
+			for (GenericValue stock : conditions) {
+				String symbol = (String) stock.get("prodSym");
+				String prodId = (String) stock.get("prodId");
+
+				if (i != 0 && i % (5 * apikey.length) == 0) {
+					Thread.sleep(65000);
+					apiindex = 0;
+					reset = true;
+				}
+				if (i != 0 && i % 5 == 0 && reset == false) {
+					apiindex++;
+				}
+				reset = false;
+
+				JSONObject resp = sendGet(symbol, apikey[apiindex]);
+				JSONObject arr = resp.getJSONObject("Monthly Adjusted Time Series");
+				SortedMap<String, String> divMap = new TreeMap<String, String>(Collections.reverseOrder());
+				Iterator it = arr.keys();
+				while (it.hasNext()) {
+					String key = (String) it.next();
+					divMap.put(key, arr.getJSONObject(key).getString("7. dividend amount"));
+				}
+				// System.out.println(divMap.toString());
+
+				boolean foundLastDiv = false;
+				boolean growCheck = true;
+				int growingDivYears = 0;
+				BigDecimal prevYearDiv = BigDecimal.ZERO;
+				Map<String, Object> yearsMap = new HashMap<String, Object>();
+				String lastClosePrice = arr.getJSONObject(divMap.firstKey()).getString("5. adjusted close");
+				System.out.print("Last closed Price" + lastClosePrice);
+
+				i++;
+			}
+
+		} catch (GenericEntityException e) {
+			Debug.logWarning(e, module);
+			Map<String, String> messageMap = UtilMisc.toMap("errMessage", e.getMessage());
+			errMsg = UtilProperties.getMessage(resource, "RefRevenueZero", messageMap, locale);
+			return ServiceUtil.returnError(errMsg);
+		}catch(InterruptedException e){
+			Debug.logWarning(e, module);
+			Map<String, String> messageMap = UtilMisc.toMap("errMessage", e.getMessage());
+			errMsg = UtilProperties.getMessage(resource, "RefRevenueZero", messageMap, locale);
+			return ServiceUtil.returnError(errMsg);
+		}catch(Exception e){
+			Debug.logWarning(e, module);
+			Map<String, String> messageMap = UtilMisc.toMap("errMessage", e.getMessage());
+			errMsg = UtilProperties.getMessage(resource, "RefRevenueZero", messageMap, locale);
+			return ServiceUtil.returnError(errMsg);
+		}
+		
+		return result;
+	}
+
+	private static JSONObject sendGet(String symbol, String apikey) throws Exception {
+
+		String url = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=" + symbol + "&apikey=" + apikey + "\"";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		// add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		// int responseCode = con.getResponseCode();
+		// System.out.println("\nSending 'GET' request to URL : " + url);
+		// System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		JSONObject jsonObj = new JSONObject(response.toString());
+
+		// print result
+		// System.out.println(jsonObj.toString());
+		// System.out.println("Symbol: " +
+		// jsonObj.getJSONObject("Meta Data").get("2. Symbol"));
+		return jsonObj;
+
+	}
+
+}
